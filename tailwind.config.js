71b0313704f5d3cafa39358821f02596da98ee87
csirona <108961876+csirona@users.noreply.github.com>
2022-10-30 18:01:08 -0300

    add tailwind in dir functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./functions/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

bc355ee5a40726a8aac65abf3fb8cc9a0d35fb75
csirona <cristianosvar@outlook.com>
2022-11-06 22:04:26 -0300

    some text changes

--- components/Homee/bio.js
+++ components/Homee/bio.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import prof from '../../assets/images/prfile-glass.jpg'
+import Link from "next/link";
 const Bio = () => {
     const [perfil, setPerfil] = useState('')
 
@@ -17,10 +18,11 @@ const Bio = () => {
                     </div>
                     <div className="card-body bg-slate-700	">
                         <p className="text-4xl">Who I am ?</p>
-                        <p>Right now I am coding, playing bass, maybe watching a football match or trying to speak japanese. But always listening music.</p>
+                        <p>Right now I'm probably coding, playing bass, watching a football game, or trying to learn Japanese right now. But I'm always listening to music.</p>
                         <div className="card-actions justify-end">
                             <p>My spotify list  </p>
-                            <button className="btn btn-primary">Listen</button>
+                            <Link href="https://open.spotify.com/playlist/1uFNBuHlxB9BhmqPzZnO2p?si=RWgDzt12R-K9JbUl3TNYzA&utm_source=copy-link"><a className="btn btn-primary">Listen</a></Link>
+                            
                         </div>
                     </div>
                 </div>

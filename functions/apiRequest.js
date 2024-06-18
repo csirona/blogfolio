de9cf61be63b96a625704e0db0171f73dd93162b
csirona <108961876+csirona@users.noreply.github.com>
2024-06-18 10:49:04 -0400

    Update apiRequest.js

--- functions/apiRequest.js
+++ functions/apiRequest.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import {useState, useEffect} from "react";
-
+import { useState, useEffect } from "react";
 
 export default function APIRequest() {
 
@@ -11,33 +10,33 @@ export default function APIRequest() {
     const response = await fetch(baseURL)
     const responseJSON = await response.json()
     setPost(responseJSON)
-
-
   }
-  
+
   useEffect(() => {
     fetchApi()
-  },[]);
-   return (
-     <div>
-         { !posts ? 'Loading... ':
-           posts.map( (post)=>{
-             return <div className="bg-slate-700 p-3 mb-5 flex-col ml-10">
+  }, []);
+
+  return (
+    <div>
+      { !posts ? 'Loading... ' :
+        posts.map((post) => {
+          return (
+            <div key={post.id} className="bg-slate-700 p-3 mb-5 flex-col ml-10">
               <Link href={'/blog/'+`${post.id}`}><a className="text-3xl ">{post.title}</a></Link>
-             <div className="text-sm ">{post.created_at}</div>
-             <div className="container flex flex-row justify-around">
-             <div className="badge badge-primary m-5">{post.tag}</div>
-             <div className='mb-3'> 
-             <Link href={post.gitlink} ><a className='link'>Link to github</a></Link>
-             </div>
-             </div>
-             
-            
-             <p className="text-base bg-slate-800">{post.content}</p>
-         </div>
-           })
-         }
-           
+              <div className="text-sm ">{new Date(post.created_at).toLocaleString()}</div>
+              <div className="container flex flex-row justify-around">
+                {post.tag.split(',').map(tag => (
+                  <div key={tag} className="badge badge-primary m-1">{tag.trim()}</div>
+                ))}
+                <div className='mb-3'> 
+                  <Link href={post.gitlink}><a className='link'>Link to GitHub</a></Link>
+                </div>
+              </div>
+              <p className="text-base bg-slate-800">{post.content}</p>
+            </div>
+          )
+        })
+      }  
     </div>
   )
 }

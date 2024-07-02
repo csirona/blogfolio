c2a4e8a9ca66e78635e82e83ffb18a2d557a3d8f
csirona <108961876+csirona@users.noreply.github.com>
2024-07-02 14:01:48 -0400

    Update bio.js

--- components/Homee/bio.js
+++ components/Homee/bio.js
@@ -18,7 +18,7 @@ const Bio = () => {
                     </div>
                     <div className="card-body bg-slate-700	">
                         <p className="text-4xl">Who I am ?</p>
-                        <p>Right now I am probably coding, playing bass, watching a football match or trying to speak Japanese. But I am always listening to music.</p>
+                        <p>Hi! I'm [Your Name], a Computer Engineer from Concepción, Chile. I have a degree in Computer Engineering and a passion for technology. While Spanish is my native language, I'm proficient in English for both personal and professional use. I have experience with Python, Go, and JavaScript, as well as their frameworks. I work with APIs and databases, applying my skills to a variety of projects. From desktop applications to web services, I enjoy creating solutions that solve real-world problems. I'm always eager to learn and grow in my field. Thank you for visiting my page. Feel free to explore my projects and reach out if you have any questions or ideas.</p>
                         <div className="card-actions justify-end">
                             <p>My spotify list  </p>
                             <Link href="https://open.spotify.com/playlist/1uFNBuHlxB9BhmqPzZnO2p?si=RWgDzt12R-K9JbUl3TNYzA&utm_source=copy-link"><a className="btn btn-primary">Listen</a></Link>
